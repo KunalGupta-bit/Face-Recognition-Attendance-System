e14af7a191ec740453058ca03a3355af031452f5
KunalGupta-bit <guptakunal8279@gmail.com>
2026-04-10 11:04:25 +0530

    deleted

--- config.py
+++
@@ -1,8 +0,0 @@
-from os import getenv
-from datetime import timedelta
-
-class Config:
-    MONGO_URI = getenv("MONGO_URI")
-    SECRET_KEY = getenv("SECRET_KEY")
-    JWT_SECRET_KEY = getenv("JWT_SECRET_KEY")
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
